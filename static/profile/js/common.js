5df97c5b1cc45584e38d5f912755a1f2d5821386
ikerya <49207045+ikerya@users.noreply.github.com>
2019-08-17 19:06:38 +0200

    Update common.js

--- static/profile/js/common.js
+++ static/profile/js/common.js
@@ -1,16 +1,51 @@
-function decodeQuery(url) {
-    var queryString = (url || "").match(/\?.+/);
+function encodeQuery(formdata, numericPrefix, argSeparator, encType) {
+    var encodeFunc = encodeURIComponent;
 
-    if (!queryString) return {};
+    var value
+    var key
+    var tmp = []
 
-    var queryString = queryString[0].substr(1);
-    var query = {};
-    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-    for (var i = 0; i < pairs.length; i++) {
-        var pair = pairs[i].split('=');
-        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    var _httpBuildQueryHelper = function(key, val, argSeparator) {
+        var k
+        var tmp = []
+        if (val === true) {
+            val = '1'
+        } else if (val === false) {
+            val = '0'
+        }
+        if (val !== null) {
+            if (typeof val === 'object') {
+                for (k in val) {
+                    if (val[k] !== null) {
+                        tmp.push(_httpBuildQueryHelper(key + '[' + k + ']', val[k], argSeparator))
+                    }
+                }
+                return tmp.join(argSeparator)
+            } else if (typeof val !== 'function') {
+                return encodeFunc(key) + '=' + encodeFunc(val)
+            } else {
+                throw new Error('There was an error processing for http_build_query().')
+            }
+        } else {
+            return ''
+        }
     }
-    return query;
+
+    if (!argSeparator) {
+        argSeparator = '&'
+    }
+    for (key in formdata) {
+        value = formdata[key]
+        if (numericPrefix && !isNaN(key)) {
+            key = String(numericPrefix) + key
+        }
+        var query = _httpBuildQueryHelper(key, value, argSeparator)
+        if (query !== '') {
+            tmp.push(query)
+        }
+    }
+
+    return tmp.join(argSeparator)
 }
 
 const user = new User(accessToken);
